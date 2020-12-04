3231e1837b53f7964668c82720187add6ef61328
CommunistLuigi <gsmith22@stu.aberdeenhall.com>
2020-12-04 14:34:30 -0800

    Added Grahamuel

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,6 @@
 package com.company;
-
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
         System.out.println("This is greatest and best program in the world");
     }
 }
